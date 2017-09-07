6e730327eb37a7bfc082e399d776ccd16d936400
Mostafa <me_psign@hotmail.com>
2017-09-07 11:24:27 +0200

    Finish user signup

--- app/controllers/users_controller (mostafa-HP-Compaq-6730s's conflicted copy 2017-09-07).rb
+++ app/controllers/users_controller (mostafa-HP-Compaq-6730s's conflicted copy 2017-09-07).rb
@@ -11,15 +11,15 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
 
   	if @user.save
-      flash[:success]= "Welcome to the Sample App!"
-  	  redirect_to user_path(@user)
+      flash[:success] = "Welcome to the Sample App!"
+  	  redirect_to @user
   	else
   	  render :new
   	end
   end
 
 
-  
+
   private
 
 	def user_params
